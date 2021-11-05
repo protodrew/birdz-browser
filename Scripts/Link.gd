5bd5af4a4323b28592688e11e7dd6027ce82d1b7
Protodrew <contact.protodrew@pm.me>
2021-11-05 11:26:02 -0400

    template additions and fixed link script

--- Scripts/Link.gd
+++ Scripts/Link.gd
@@ -1,8 +1,11 @@
-extends Area2D
+extends Node2D
 # a simple script that allows a custom event to execute when the area2D defined is clicked
 
-onready var navhandler = self.get_parent().get_parent(); # moves 2 levels up in the hierarchy to interact with the next level
+onready var navhandler = self.get_parent().get_parent() # moves 2 levels up in the hierarchy to interact with the next level
+onready var collider = $Area2D
+
 # TODO the implementation was broken somehow, will fix after dinner
-func _on_Link_input_event(_viewport, event, _shape_idx): # underscores mean it won't throw errors
+func _on_Area2D_input_event(_viewport, event, _shape_idx): # underscores mean it won't throw errors
 	if (event is InputEventMouseButton && event.pressed): # ensures we are inside the area and clicking
 		print("clicked")
+		# TODO make a group based change system
