648362886d8240bde0ae9ee8c51567cbccd8a98e
Protodrew <pull.rebase>
2021-11-08 17:24:26 -0500

    2D navigation

--- Scripts/2D Navigation.gd
+++ Scripts/2D Navigation.gd
@@ -2,10 +2,15 @@ extends Node2D
 
 
 # This will be used to transfer pages to other pages in the 2D web sections of the game
-
-func movepage():
-	print("groupstest")
+func movepage(var pgto):
+	
+	var page = "res://Sites/"+pgto+".tscn"
+	print(page)
+	
+	clear()
+	add_child(load(page).instance())
 
 func clear():
 	for n in self.get_children(): # loops through all children of the 2D Navigation Scene
 		self.remove_child(n) # removes child from tree
+		
