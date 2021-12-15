efb5796d9c4cf4480aa984304bd44b8d58ac1bc7
Protodrew <contact.protodrew@pm.me>
2021-12-15 01:00:01 -0500

    splash screen & menu alpha

--- Scripts/Link.gd
+++ Scripts/Link.gd
@@ -8,13 +8,12 @@ onready var groups = self.get_groups()
 
 func _on_Area2D_input_event(_viewport, event, _shape_idx): # underscores mean it won't throw errors
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1: # ensures we are inside the area and clicking
+		print("balls")
 		var moved = false
 		for index in groups.size():
 			if "page" in groups[index]:
 				var pgname = groups[index].substr(5)
 				navhandler.movepage(pgname)
-					
-					
 				moved = true
 		if !moved:
 			print("error no page group")
