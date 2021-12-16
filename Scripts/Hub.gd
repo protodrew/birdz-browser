86fb3d1aa65c71de94aa6de218fab58b75c63d82
Protodrew <contact.protodrew@pm.me>
2021-12-16 04:01:19 -0500

    hub navigation rework

--- Scripts/Hub.gd
+++ Scripts/Hub.gd
@@ -3,12 +3,16 @@ onready var camera = $Camera2D
 var pressed = false
 var start = Vector2.ZERO
 var current = Vector2.ZERO
+var dragging = false
 func _ready():
   set_process_input(true)
 
-func _process(_delta):
-	if Input.is_action_just_pressed("click"):
-		start = get_viewport().get_mouse_position()
-	if Input.is_action_pressed("click"):
-		current = (start - get_viewport().get_mouse_position())
-		camera.position += Vector2(sign(current.x)*5,sign(current.y)*5)
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			dragging = true
+		else:
+			dragging = false
+	elif event is InputEventMouseMotion and dragging:
+		camera.position.x = move_toward(camera.position.x,get_global_mouse_position().x,15)
+		camera.position.y = move_toward(camera.position.y,get_global_mouse_position().y,15)
